5bde0e300f04a8badc4f08ba01e76c012f9589b7
Aspire <aspiremac@Apples-MacBook-Pro.local>
2018-08-29 18:10:21 +0530

    Resonance Sample IOS App Added

---
+++ resonanceSampleIOS/resonanceSample/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  resonanceSample
+//
+//  Created by ManiPalanivel on 12/06/18.
+//  Copyright © 2018 ManiPalanivel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)setupAudioButtonTapped:(id)sender;
+- (IBAction)startSearchButtonTapped:(id)sender;
+- (IBAction)stopSearchButtonTapped:(id)sender;
+
+
+@end
+
